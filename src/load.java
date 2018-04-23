3303e395c23b59add8907abfe172a0f7c8058a8f
mansey7 <33343202+mansey7@users.noreply.github.com>
2018-04-23 17:01:05 +0100

    added the load function

--- src/load.java
+++ src/load.java
@@ -9,3 +9,10 @@ public class Load {
 
     public Load() {
     }
+    
+    public Load(int x, int y, String imagePath) {//function to load images onto screen
+        this.x = x;
+        this.y = y;
+        this.image = Util.loadImage(imagePath);
+    }
+}
