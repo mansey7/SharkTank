3edb15eaea6f1bfb6a791572ea1b08d8a5c628f3
LukeBoi97 <afroman97lm@gmail.com>
2018-04-23 16:36:54 +0100

    Update Shark.java
    declared public variables

--- Shark.java
+++ Shark.java
@@ -1,4 +1,18 @@
+import java.awt.Image;
+import java.awt.event.KeyEvent;
+import java.awt.geom.AffineTransform;
+
 public class Shark {
+
+    public int x;
+    public int y;
+    public int width;
+    public int height;
+
+    public boolean dead;
+
+    public double yvel;
+    public double gravity;
 
 
 
