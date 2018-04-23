cb61e6236a36f9b84f1278192181162a0de4acaa
mansey7 <33343202+mansey7@users.noreply.github.com>
2018-04-23 16:59:59 +0100

    created load.java

---
+++ src/load.java
@@ -0,0 +1,11 @@
+import java.awt.Image;
+import java.awt.geom.AffineTransform;
+
+public class Load {
+    public int x;
+    public int y;
+    public Image image;
+    public AffineTransform transform;//Java function which allows for the parallelness and straightness of lines
+
+    public Load() {
+    }
