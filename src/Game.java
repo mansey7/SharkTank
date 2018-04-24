e4cb605220ec400cc9a1d6992b0690ad15efaa33
mansey7 <33343202+mansey7@users.noreply.github.com>
2018-04-24 16:52:25 +0100

    added function to display and move seaweeds

--- src/Game.java
+++ src/Game.java
@@ -24,6 +24,7 @@ public class Game {
 
         if (gameover)
             return;
+         moveSeaweeds();
       }
     
     public void restart() {//resets all the variables , ready for new start of game
@@ -53,6 +54,45 @@ public class Game {
             restart();
             restartDelay = 50;// this causes a delay on the restart button so it cannot be spammed
             return;
+        }
+    }
+    private void moveSeaweeds() {
+        seaweedDelay--;
+        if (seaweedDelay < 0) {
+            seaweedDelay = SEAWEED_DELAY;
+            Seaweed northSeaweed = null;
+            Seaweed southSeaweed = null;
+
+            // Look for Seaweed off the screen
+            for (Seaweed seaweed : seaweeds) {
+                if (seaweed.x - seaweed.width < 0) {
+                    if (northSeaweed == null) {
+                        northSeaweed = seaweed;
+                    } else if (southSeaweed == null) {
+                        southSeaweed = seaweed;
+                        break;
+                    }
+                }
+            }//creates a north and south seaweed if they are not onscreen
+            if (northSeaweed == null) {
+            	Seaweed seaweed = new Seaweed("north");
+            	seaweeds.add(seaweed);
+                northSeaweed = seaweed;
+            } else {
+                northSeaweed.reset();
+            }
+            if (southSeaweed == null) {
+            	Seaweed seaweed = new Seaweed("south");
+            	seaweeds.add(seaweed);
+                southSeaweed = seaweed;
+            } else {
+                southSeaweed.reset();
+            }
+            //sets the distance between the seaweeds
+            northSeaweed.y = southSeaweed.y + southSeaweed.height + 175;
+        }//calls function to move seaweed
+        for (Seaweed seaweed : seaweeds) {
+        	seaweed.update();
         }
     }
     
