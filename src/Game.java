deef3b748c6ca11435e51b0ac6ae0021c0545c23
mansey7 <33343202+mansey7@users.noreply.github.com>
2018-04-24 14:01:42 +0100

    Game class and game running function

---
+++ src/Game.java
@@ -0,0 +1,28 @@
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+
+public class Game {
+    private Shark shark;
+    private Keyboard keyboard;
+
+    public int score;
+    public Boolean gameover;
+    public Boolean started;
+
+    public Game() {
+        keyboard = Keyboard.getInstance();
+        restart();
+    }
+    public void update() {
+        watchForStart();
+
+        if (!started)
+            return;
+
+        shark.update();
+
+        if (gameover)
+            return;
+      }
+    
+}
