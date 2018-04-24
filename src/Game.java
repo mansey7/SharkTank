a23f4f6562ebe135dc8947a70cbf50b570a808fc
mansey7 <33343202+mansey7@users.noreply.github.com>
2018-04-24 14:24:38 +0100

    added the restart of game option

--- src/Game.java
+++ src/Game.java
@@ -17,16 +17,43 @@ public class Game {
         watchForStart();
 
         if (!started)
-            return;
+            return;//update() will loop around to this point until game is started
 
-        shark.update();
+        shark.update();// calls function to update the shark position on screen
+        watchForReset();
 
         if (gameover)
             return;
       }
+    
+    public void restart() {//resets all the variables , ready for new start of game
+      
+        started = false;
+        gameover = false;
+
+        score = 0;
+ 
+        restartDelay = 0;
+        seaweedDelay = 0;
+
+        shark = new Shark();
+        seaweeds = new ArrayList<Seaweed>();
+    }
+    
     private void watchForStart() {
-        if (!started && keyboard.isDown(KeyEvent.VK_SPACE)) {
+        if (!started && keyboard.isDown(KeyEvent.VK_SPACE)) {//checks for start of the game by press of the space bar
             started = true;
         }
     }
+    private void watchForReset() {
+        if (restartDelay > 0)
+            restartDelay--;//decrements the restart time
+
+        if (keyboard.isDown(KeyEvent.VK_R) && restartDelay <= 0) {
+            restart();
+            restartDelay = 50;// this causes a delay on the restart button so it cannot be spammed
+            return;
+        }
+    }
+    
 }
