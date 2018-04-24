2d31cd4f78082c22aeb25a7ff8263414509ec518
LukeBoi97 <afroman97lm@gmail.com>
2018-04-24 15:41:09 +0100

    created the seaweed class

---
+++ src/Seaweed.java
@@ -0,0 +1,16 @@
+import java.awt.Image;
+
+public class Seaweed {
+
+    public int x;
+    public int y;
+    public int width;
+    public int height;
+    public int speed = 5;
+
+    public String position;
+
+    private Image image;
+
+    
+}
